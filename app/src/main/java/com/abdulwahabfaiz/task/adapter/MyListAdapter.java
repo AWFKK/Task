3e60d6805c1e6f8e98a05d42ee0b853fc21f58fa
AWFKK <abdulwahabfaiz@gmail.com>
2020-12-19 01:35:07 +0500

    Final

--- app/src/main/java/com/abdulwahabfaiz/task/adapter/MyListAdapter.java
+++ app/src/main/java/com/abdulwahabfaiz/task/adapter/MyListAdapter.java
@@ -3,10 +3,12 @@ package com.abdulwahabfaiz.task.adapter;
 import android.content.Context;
 import android.content.Intent;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,9 +28,7 @@ import java.util.ArrayList;
 public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ViewHolder> {
     private ArrayList<MyListData> listdata;
     Context mContext;
-
-    // RecyclerView recyclerView;
-
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
 
     public MyListAdapter(ArrayList<MyListData> listdata, Context mContext) {
         this.listdata = listdata;
@@ -47,6 +47,7 @@ public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ViewHolder
     public void onBindViewHolder(ViewHolder holder, int position) {
         final MyListData arraylist = listdata.get(position);
         holder.textView.setText(arraylist.getDescription());
+        holder.mainLayout.setVisibility(View.VISIBLE);
 //        holder.imageView.setImageResource(arraylist.getImgId());
 
         long bidTime = arraylist.getTime();
@@ -65,11 +66,24 @@ public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ViewHolder
             // there and
             public void onFinish() {
                 holder.lottieAnimationView.setVisibility(View.VISIBLE);
+                holder.mainLayout.setVisibility(View.INVISIBLE);
                 holder.txtTimer.setText("00:00:00");
                 Toast.makeText(mContext, "Bid Finish "+arraylist.getDescription()+" Removed", Toast.LENGTH_SHORT).show();
                 try {
                     listdata.remove(position);
-                    notifyDataSetChanged();
+
+                    /* New Handler to start the Menu-Activity
+                     * and close this Splash-Screen after some seconds.*/
+                    new Handler().postDelayed(new Runnable(){
+                        @Override
+                        public void run() {
+                            /* Create an Intent that will start the Menu-Activity. */
+                            notifyDataSetChanged();
+                            holder.lottieAnimationView.setVisibility(View.INVISIBLE);
+                        }
+                    }, SPLASH_DISPLAY_LENGTH);
+
+
                 }catch (Exception e){}
             }
         }.start();
@@ -78,8 +92,6 @@ public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ViewHolder
             @Override
             public void onClick(View view) {
                 Toast.makeText(view.getContext(), "click on item: " + arraylist.getDescription(), Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(mContext, Timer.class);
-                mContext.startActivity(intent);
             }
         });
     }
@@ -95,6 +107,7 @@ public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ViewHolder
         public TextView textView, txtTimer;
         public CardView card;
         public LottieAnimationView lottieAnimationView;
+        public LinearLayout mainLayout;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -103,6 +116,7 @@ public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ViewHolder
             this.txtTimer   =   itemView.findViewById(R.id.txt_timer);
             card            =   itemView.findViewById(R.id.cardView);
             lottieAnimationView = itemView.findViewById(R.id.animationView);
+            mainLayout = itemView.findViewById(R.id.main);
         }
     }
 
